15034681e812b06a421003df3870617948d390ee
Mihai Flavius <am_ban_pe_github@yahoo.com>
2026-04-21 20:43:20 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+CATEGORIES = ["IT", "Software Engineer", "Technical Support", "Data", "Cybersecurity", "DevOps"]
+SITES = ["linkedin", "indeed"]
+LOCATION = "romania" # trebuie lowercase
+RESULTS_PER_PAGE = 30
+HOURS_OLD = 72  
+
+DISPLAY_COLUMNS = ['title', 'company', 'location', 'date_posted', 'job_url']
+
+RO_SITES = {
+    "juniors": "https://juniors.ro/jobs",
+    "devjob": "https://devjob.ro"
+}
